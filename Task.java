013d42e6bd11119ff2985c8e3f3ce4c53fc54c74
SiddhapuraNikita <siddhapuraniku@gmail.com>
2024-06-11 13:56:50 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class Task {
+    public static ArrayList<Integer> list;
+
+    public Task(){
+       list=new ArrayList<>();
+    }
+    public void addGrade(int grade){
+        list.add(grade);
+    }
+
+    public double calculateAverage(){
+        if(list.isEmpty()){
+            return 0.0;
+        }
+        int sum=0;
+        for(int i:list){ 
+            sum = sum+ i;
+        }
+        return (double)sum/list.size();
+    }
+
+    public int getHighestGrade(){
+        if(list.isEmpty()){
+            return 0;
+        }
+        int h=list.get(0);
+        for(int i : list){
+            if(h<i){
+                h=i;
+            }
+        }
+        return h;
+    }
+
+    public int getLowestGrade(){
+        if(list.isEmpty()){
+            return -1;
+        }
+        int l=list.get(0);
+        for(int i : list){
+            if(l>i){
+                l=i;
+            }
+        }
+        return l;
+    }
+    public static void main(String[] args) 
+    {
+       Task t=new Task();
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter no. of elements:");
+       int a=sc.nextInt();
+       System.out.println("");
+
+        try{
+            for(int i=0; i<a; i++)
+            {
+              list.add(sc.nextInt());
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+        double ave= t.calculateAverage();
+        int high=t.getHighestGrade();
+        int low=t.getLowestGrade();
+
+        System.out.println("Average of Grades= "+ave);
+        System.out.println("Highest Grades is= "+high);
+        System.out.println("Lowest Grades is= "+low);
+    }
+}
